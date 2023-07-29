e57728b048b3abaa43cab58ae14108360e01810c
Gaurang105 <gujratigaurang@gmail.com>
2023-07-29 14:12:48 +0530

    q-20

---
+++ RomanToInteger.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        n = len(s)
+        roman={"I":1,
+               "V":5,
+               "X":10,
+               "L":50,
+               "C":100,
+               "D":500,
+               "M":1000}
+
+        number = 0
+        for i in range(n-1):
+            if roman[s[i]] < roman[s[i+1]]:
+                number -= roman[s[i]]
+            else:
+                number += roman[s[i]]
+        return number + roman[s[-1]]
