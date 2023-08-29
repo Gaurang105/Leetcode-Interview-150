a23c994c306b2543686ec93a0167bad26488fab3
Gaurang105 <gujratigaurang@gmail.com>
2023-08-29 22:15:07 +0530

    q-45

---
+++ Sorting Algos/BubbleSort.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def bubbleSort(arr: List[int], n: int):
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
